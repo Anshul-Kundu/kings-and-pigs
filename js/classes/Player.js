e1d42769126b13ca8a2a136d034e92b112154f06
Anshul-Kundu <106953152+Anshul-Kundu@users.noreply.github.com>
2024-07-15 11:26:44 +0530

    hitbox added

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -27,9 +27,12 @@ class Player extends Sprite{
         //c.fillStyle='rgba(0,0,255,0.5)'
         //c.fillRect(this.position.x,this.position.y,this.width,this.height)
         this.position.x+=this.velocity.x
-
+        this.updateHitbox()
         this.checkForHorizontalCollisions()
+    
         this.applyGravity()
+
+        this.updateHitbox()
         this.checkForVerticalCollisions()
         
     }
@@ -39,47 +42,62 @@ class Player extends Sprite{
             const collisionBlock =this.collisionBlocks[i]
             //if a collision exits
             if(
-                this.position.x<=collisionBlock.position.x+collisionBlock.width &&
-                this.position.x+this.width>=collisionBlock.position.x &&
-                this.position.y+this.height>=collisionBlock.position.y &&
-                this.position.y<=collisionBlock.position.y+collisionBlock.height
+                this.hitbox.position.x<=collisionBlock.position.x+collisionBlock.width &&
+                this.hitbox.position.x+this.hitbox.width>=collisionBlock.position.x &&
+                this.hitbox.position.y+this.hitbox.height>=collisionBlock.position.y &&
+                this.hitbox.position.y<=collisionBlock.position.y+collisionBlock.height
             ){
                 //collision on the x-axis going left
                 if(this.velocity.x<0){
-                    this.position.x=collisionBlock.position.x+collisionBlock.width+0.01
+                    const offset=this.hitbox.position.x-this.position.x
+                    this.position.x=collisionBlock.position.x+collisionBlock.width-offset+0.01
                     break
                 }
                 if(this.velocity.x>0){
-                    this.position.x=collisionBlock.position.x-this.width-0.01
+                    const offset=this.hitbox.position.x-this.position.x+this.hitbox.width
+                    this.position.x=collisionBlock.position.x-offset-0.01
                     break
                 }
             }
         }
     }
 
-    
+
     checkForVerticalCollisions(){
         for(let i=0; i<this.collisionBlocks.length;i++){
             const collisionBlock =this.collisionBlocks[i]
             //if a collision exits
             if(
-                this.position.x<=collisionBlock.position.x+collisionBlock.width &&
-                this.position.x+this.width>=collisionBlock.position.x &&
-                this.position.y+this.height>=collisionBlock.position.y &&
-                this.position.y<=collisionBlock.position.y+collisionBlock.height
+                this.hitbox.position.x<=collisionBlock.position.x+collisionBlock.width &&
+                this.hitbox.position.x+this.hitbox.width>=collisionBlock.position.x &&
+                this.hitbox.position.y+this.hitbox.height>=collisionBlock.position.y &&
+                this.hitbox.position.y<=collisionBlock.position.y+collisionBlock.height
             ){
                 //collision on the x-axis going left
                 if(this.velocity.y<0){
+                    const offset=this.hitbox.position.y-this.position.y
                     this.velocity.y=0
-                    this.position.y=collisionBlock.position.y+collisionBlock.height+0.01
+                    this.position.y=collisionBlock.position.y+collisionBlock.height-offset+0.01
                     break
                 }
                 if(this.velocity.y>0){
+                    const offset=this.hitbox.position.y-this.position.y+this.hitbox.height
                     this.velocity.y=0
-                    this.position.y=collisionBlock.position.y-this.height-0.01
+                    this.position.y=collisionBlock.position.y-offset-0.01
                     break
                 }
             }
+        }
+    }
+
+    updateHitbox(){
+        this.hitbox={
+            position:{
+                x:this.position.x+58,
+                y:this.position.y+34
+            },
+            width:50,
+            height:53
         }
     }
 
